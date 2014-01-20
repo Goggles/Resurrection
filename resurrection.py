4e7ac9412c810e738e427b8f741e372705cc62b3
Goggles <yarrowpb@gmail.com>
2014-01-20 15:05:14 +0000

    Added a help command

--- resurrection.py
+++ resurrection.py
@@ -6,7 +6,9 @@ memories_map = [
 	(
 """You step out with Death from the warm office into a place that smells distinctly like dust and dry paper, utterly devoid of sound. Until Death says 'This corridor is an offshoot of the Maze of Memories. The memories of the dead exist here - in theory this place contains the memories we need.'""", "First Corridor of maze"), #0
 	(
-"""Death leads the way down the corridor to a dead end. 'Let me just...' and with one sweep of an arm, a word appears on the wall. """, "FirstPuzzle"), #1
+"""Death leads the way down the corridor to a dead end. 'Let me just...' and with one sweep of an arm, a word appears on the wall. """, "Corridor Part 1"), #1
+	(
+"""""", "")
 ]
 
 
@@ -17,6 +19,15 @@ memories_map = [
 
 while True:
 	command = raw_input("> ")
+
+	if command == "help":
+		print """help: brings up list of commands
+exit, quit: quit the game.
+up: move up
+down: move down
+right: move right
+left: move left
+"""
 
 	if command == "exit" or command == "quit":
 		break
